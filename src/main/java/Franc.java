a60c7f15d633ca3291a86aa223eaf1cbc840b0d2
Wesley <wesleyvc2110@gmail.com>
2023-04-16 14:53:55 -0300

    third step

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -3,9 +3,10 @@ class Franc extends Money {
 
     Franc(int amount) {      
        this.amount= amount;
-    }					
-    Franc times(int multiplier)  {      
-       return new Franc(amount * multiplier);					
-    }   
+    }
+    					
+    Money times(int multiplier)  {
+        return new Franc(amount * multiplier);
+    }
 		
 }
